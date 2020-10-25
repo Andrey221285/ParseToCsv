4dcb4b0d482bc008983567bf50b2d30689686972
AndreyErmolaev <andrey221985@gmail.com>
2020-10-25 19:41:56 +0700

    save

--- src/Pars.java
+++ src/Pars.java
@@ -11,34 +11,34 @@ public class Pars {
 
     public static void main(String[] args) {
         Date date = new Date();
-       try (BufferedReader reader = new BufferedReader(new FileReader(input));
-       BufferedWriter writer = new BufferedWriter(new FileWriter(output + formater.format(date) + ".csv"))){
-           writer.write(head + "\n");
-           int count = 1;
-           String line = "" +  count;
-           int countRead = 0;
-           while (reader.ready()){
-               String s = reader.readLine();
-               if (s.isEmpty()){
-                   continue;
-               }
-               line += ";" + s;
-               countRead ++;
-               if (countRead == 1){
-                   line += ";" + action;
-               }else if (countRead > 2){
+        try (BufferedReader reader = new BufferedReader(new FileReader(input));
+             BufferedWriter writer = new BufferedWriter(new FileWriter(output + formater.format(date) + ".csv"))){
+            writer.write(head + "\n");
+            int count = 1;
+            String line = "" +  count;
+            int countRead = 0;
+            while (reader.ready()){
+                String s = reader.readLine();
+                if (s.isEmpty()){
+                    continue;
+                }
+                line += ";" + s;
+                countRead ++;
+                if (countRead == 1){
+                    line += ";" + action;
+                }else if (countRead > 2){
 
-                   writer.write(line + "\n");
-                   count++;
-                   countRead = 0;
-                   line = "" + count;
-               }
-           }
+                    writer.write(line + "\n");
+                    count++;
+                    countRead = 0;
+                    line = "" + count;
+                }
+            }
 
-       } catch (FileNotFoundException e) {
-           e.printStackTrace();
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
